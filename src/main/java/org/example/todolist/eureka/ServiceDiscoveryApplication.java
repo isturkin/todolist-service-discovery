720b9119a3abed354ecfd9c5c96e911816443da4
ivanturkin <ivanturkinrussia@gmail.com>
2024-04-01 17:25:51 +0300

    change package names

--- src/main/java/org/example/todolist/eureka/ServiceDiscoveryApplication.java
+++ src/main/java/org/example/todolist/eureka/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package todolist.eureka;
+package org.example.todolist.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
